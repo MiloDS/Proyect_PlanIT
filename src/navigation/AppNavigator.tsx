af584c857110f57c660c0390ab617ff2861b5179
MiloDS <152004293+MiloDS@users.noreply.github.com>
2026-09-12 11:39:02 -0500

    feat(project): initial commit with project architecture and README

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,18 @@
+import React from 'react-native';
+import{NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {HomeScreen} from '../screens/HomeScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+export function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text, Pressable} from 'react-native';
+
+export function HomeScreen() {
+  return (
+    <View>
+      <Text>
+      </Text>
+
+    </View>
+  );
+};
+
+
